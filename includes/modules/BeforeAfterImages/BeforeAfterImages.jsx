b10fd796bf5b7f283c5d38ddb2e31d4a1dbc4cb8
Aaron Bolton <aaron@boltonstudios.com>
2020-05-10 22:48:55 -0400

    Resolved bug when switching sizes to full size

--- includes/modules/BeforeAfterImages/BeforeAfterImages.jsx
+++ includes/modules/BeforeAfterImages/BeforeAfterImages.jsx
@@ -7,21 +7,6 @@ import {createImageSrcAtSize} from './helpers.js';
 import {generateStyles} from './helpers.js';
 import {getOrientationClasses} from './helpers.js';
 
-class Image extends Component{
-    render(){
-        return(
-            <img
-                onLoad={this.props.onLoad}
-                src={this.props.src}
-                alt={this.props.alt}
-                className={this.props.className}
-                style={this.props.style}
-                width={this.props.width}
-                height={this.props.height}
-            />
-        );
-    }
-}
 class Overlay extends Component{
     render(){
         return(
@@ -51,83 +36,103 @@ class BeforeAfterImages extends Component {
         super(props);
         this.state = {
             bDimensions: {}, // Before Image
-            aDimensions: {} // After Image
+            aDimensions: {}, // After Image
+            bOriginalDimensions: {}, // Before Image
+            aOriginalDimensions: {} // After Image
         };
         this.onBeforeImgLoad = this.onBeforeImgLoad.bind(this);
         this.onAfterImgLoad = this.onAfterImgLoad.bind(this);
 
         // Create callback refs
         this.container = React.createRef();
+        this.containerFullSize = {
+            width: 0,
+            height: 0
+        }
     }
     onBeforeImgLoad({ target: img }) {
+        let dimensions = {};
+        const currentDimensions = {
+            beforeOffsetHeight: img.offsetHeight,
+            beforeOffsetWidth: img.offsetWidth,
+            beforeNaturalHeight: img.naturalHeight,
+            beforeNaturalWidth: img.naturalWidth,
+        }
+        if( typeof(this.state.bOriginalDimensions.beforeOffsetWidth) === ("" || undefined || "undefined" || null) ){
+            dimensions = currentDimensions;
+        } else{
+            dimensions = this.state.bOriginalDimensions;
+        }
         this.setState({
-            bDimensions: {
-                beforeOffsetHeight: img.offsetHeight,
-                beforeOffsetWidth: img.offsetWidth,
-                beforeNaturalHeight: img.naturalHeight,
-                beforeNaturalWidth: img.naturalWidth,
-            }
+            bDimensions: currentDimensions,
+            bOriginalDimensions: dimensions
         });
     }
     onAfterImgLoad({ target: img }) {
+        let dimensions = {};
+        const currentDimensions = {
+            afterOffsetHeight: img.offsetHeight,
+            afterOffsetWidth: img.offsetWidth,
+            afterNaturalHeight: img.naturalHeight,
+            afterNaturalWidth: img.naturalWidth,
+        }
+        if( typeof(this.state.aOriginalDimensions.afterOffsetWidth) === ("" || undefined || "undefined" || null) ){
+            dimensions = currentDimensions;
+        } else{
+            dimensions = this.state.aOriginalDimensions;
+        }
         this.setState({
-            aDimensions: {
-                afterOffsetHeight: img.offsetHeight,
-                afterOffsetWidth: img.offsetWidth,
-                afterNaturalHeight: img.naturalHeight,
-                afterNaturalWidth: img.naturalWidth,
-            }
+            aDimensions: currentDimensions,
+            aOriginalDimensions: dimensions
         });
     }
     render() {
+        
+        /**
+         * Get user-defined settings.
+         */
 
-        // Create images objects from user input: image source.
-        var beforeImage = new Image();
-        var afterImage = new Image();
+        // Create images objects to store image source and other data.
+        let beforeImage = new Image();
+        let afterImage = new Image();
+
+        // Get settings from props.
         beforeImage.src = this.props.src_before;
         afterImage.src = this.props.src_after;
-        
-        // Store the natural image dimensions.
-        const { beforeOffsetWidth, beforeOffsetHeight, beforeNaturalWidth, beforeNaturalHeight  } = this.state.bDimensions;
-        const { afterOffsetWidth, afterOffsetHeight, afterNaturalWidth, afterNaturalHeight  } = this.state.aDimensions;
-
-        // Used in logic where the user selected the "Full size" size option.
-        const fullSizeWidth = (beforeNaturalWidth <= afterNaturalWidth) ? beforeNaturalWidth : afterNaturalWidth;
-        const fullSizeHeight = (beforeNaturalHeight <= afterNaturalHeight) ? beforeNaturalHeight : afterNaturalHeight;
-        //const fullSizeWidth = imgNaturalWidth;
-        //const fullSizeHeight = imgNaturalHeight;
-        
-        // Get labels from props.
         const labels = {
             before: this.props.label_before,
             after: this.props.label_after
         }
-
-        // Get slider offset from props.
         const sliderOffset = {
             string: this.props.slider_offset,
             int:  parseInt(this.props.slider_offset)/100
         }
-
-        // Get user input: size.
-        var sizeSelected = this.props.size;
+        const sizeSelected = this.props.size;
+        const alignment = this.props.align;
+        
+        // Store the natural image dimensions.
+        const { beforeOffsetWidth, beforeOffsetHeight, beforeNaturalWidth, beforeNaturalHeight  } = this.state.bDimensions;
+        const { afterOffsetWidth, afterOffsetHeight, afterNaturalWidth, afterNaturalHeight  } = this.state.aDimensions;
+     
+        // Used in logic where the user selected the "Full size" size option.
+        const fullSizeWidth = (beforeNaturalWidth <= afterNaturalWidth) ? beforeNaturalWidth : afterNaturalWidth;
+        const fullSizeHeight = (beforeNaturalHeight <= afterNaturalHeight) ? beforeNaturalHeight : afterNaturalHeight;
 
         // Check if the user declined to select a size.
-        var didSelectSize = (sizeSelected === undefined || sizeSelected === 'undefined' || sizeSelected === 'selectasize.') ? false : true;
+        const didSelectSize = (sizeSelected === undefined || sizeSelected === 'undefined' || sizeSelected === 'selectasize.') ? false : true;
         
         // Check if the user selected "Full Size."
-        var didSelectFullSize = ( sizeSelected === 'full' || sizeSelected === 'fullsize.' ) ? true : false;
+        const didSelectFullSize = ( sizeSelected === 'full' || sizeSelected === 'fullsize.' ) ? true : false;
 
         // Initialize other variables.
-        var selectedSizeAttributes = {
+        let selectedSizeAttributes = {
             width: 0,
             height: 0,
             didSelectFullSize: didSelectFullSize
         };
-        var selectedWidth = ''; 
-        var selectedHeight = '';
-        var sizeClass = '';
-        var alignment = this.props.align;
+        let selectedWidth = ''; 
+        let selectedHeight = '';
+        let sizeClass = '';
         
         // Set some classes for the wrapper based on the image size.
         if( sizeSelected === 'undefined' || sizeSelected === 'selectasize.' ){
@@ -136,11 +141,16 @@ class BeforeAfterImages extends Component {
             sizeClass = 'sizeFull';
         }
         
+        /**
+         * App logic to generate images.
+         */
+        
         // Reconstruct image URL to retrieve image at selected size.
         if ( didSelectSize === true && didSelectFullSize === false ){
+
+            // User selected an image size other than "full size".
             
-            // Construct image source URL's at selected size.
-            // Also get the selected image size for the module.
+            // Get image source size and URL's at selected size.
             var beforeImageAtSize = createImageSrcAtSize( beforeImage.src, sizeSelected );
             var afterImageAtSize = createImageSrcAtSize( afterImage.src, sizeSelected )
 
@@ -170,6 +180,8 @@ class BeforeAfterImages extends Component {
 
         } else if( didSelectSize === false && didSelectFullSize === false  ) {
 
+            // User did not select an image size.
+
             // No size selected
             sizeClass = getOrientationClasses( beforeImage, afterImage );
             selectedWidth = 376;
@@ -180,21 +192,45 @@ class BeforeAfterImages extends Component {
             selectedSizeAttributes.height = selectedHeight;
 
         } else{
-            
-            // Full size selected.
+
+            // User selected an image size: "full size".
+
             sizeClass = getOrientationClasses( beforeImage, afterImage, ['sizeFull'] );
-            selectedWidth = ( beforeImage.width > afterImage.width ) ? afterImage.width : beforeImage.width;
-            selectedHeight = ( beforeImage.height > afterImage.height ) ? afterImage.width : beforeImage.width;
+
+            // Default: Use the natural dimensions of the image.
+            selectedWidth = ( beforeNaturalWidth < afterNaturalWidth ) ? beforeNaturalWidth : afterNaturalWidth;
+            selectedHeight = ( beforeNaturalHeight < afterNaturalHeight) ? beforeNaturalHeight : afterNaturalHeight;
+
+            // Set the image dimensions to the bounds of the container.
+            if( this.container.current !== null ){
+                
+                // Get the container and image attributes.
+                const orientation = (fullSizeWidth >= fullSizeHeight) ? 'landscape' : 'portrait';
+                const aspectRatio = (orientation === 'landscape') ? fullSizeHeight/fullSizeWidth : fullSizeWidth/fullSizeHeight;
+                selectedWidth = this.container.current.clientWidth;
+                selectedHeight = Math.trunc(this.container.current.clientWidth * aspectRatio);
+            }
+
+            // Set the image dimensions to the bounds of the container.
+            if( this.state.bOriginalDimensions.beforeOffsetWidth > 0 ){
+
+                // Get the container and image attributes.
+                const bWidth = this.state.bOriginalDimensions.beforeOffsetWidth;
+                const bHeight = this.state.bOriginalDimensions.beforeOffsetHeight;
+                const aWidth = this.state.aOriginalDimensions.afterOffsetWidth;
+                const aHeight = this.state.aOriginalDimensions.afterOffsetHeight;
+                const startingFullSizeWidth = (bWidth <= aWidth) ? bWidth : aWidth;
+                const startingFullSizeHeight = (bHeight <= aHeight) ? bHeight : aHeight;
+
+                selectedWidth = startingFullSizeWidth;
+                selectedHeight = startingFullSizeHeight;
+            }
 
             // Update selectedSizeAttributes
             selectedSizeAttributes.width = selectedWidth;
             selectedSizeAttributes.height = selectedHeight;
         }
-        
-        // Classes
-        var wrapperClasses = "twentytwenty-wrapper twentytwenty-horizontal et_pb_image_wrap " + sizeClass;
-        var containerClasses = "twentytwenty-container";
-        
+
         // Styles
         var styles = generateStyles(
             alignment,
@@ -202,23 +238,6 @@ class BeforeAfterImages extends Component {
             sliderOffset.int,
             sliderOffset.string
         );
-
-        // Adjust styles if the user selected "Full size".
-        if(this.container.current !== null && didSelectFullSize === true ){
-
-            // Get the container and image attributes.
-            var orientation = (fullSizeWidth >= fullSizeHeight) ? 'landscape' : 'portrait';
-            var aspectRatio = (orientation === 'landscape') ? fullSizeHeight/fullSizeWidth : fullSizeWidth/fullSizeHeight;
-            var width = this.container.current.clientWidth;
-            var height = Math.trunc(this.container.current.clientWidth * aspectRatio);
-
-            // Apply changes to styles object.
-            styles.container.height = height;
-            styles.beforeImage.height = height;
-            styles.beforeImage.clip = 'rect(0px, ' + (width*sliderOffset.int) + 'px, ' + height + 'px, 0px)'
-            styles.afterImage.height = height;
-            styles.afterImage.clip = 'rect(0px, ' + width + 'px, ' + height + 'px, ' + (width*sliderOffset.int) + 'px)'
-        }
         return (
             <Fragment>
                 <div id="et-boc">
@@ -234,20 +253,28 @@ class BeforeAfterImages extends Component {
                         <li>After Natural Width: {afterNaturalWidth}</li>
                         <li>After Natural Height: {afterNaturalHeight}</li>
                     </ul>
-                    <div class="et-l">
-                        <div class={wrapperClasses} style={styles.wrapper} ref={this.wrapper}>
-                            <div class={containerClasses} style={styles.container} ref={this.container}>
-                                <Image
-                                    onLoad = {this.onBeforeImgLoad}
+                    <ul>
+                        <li>Before Image Width: {beforeImage.width}</li>
+                        <li>Before Image Height: {beforeImage.height}</li>
+                        <li>Before Image Natural Width: {beforeImage.naturalWidth}</li>
+                        <li>Before Image Natural Height: {beforeImage.naturalHeight}</li>
+                        <li>Before Image Offset Width: {beforeImage.offsetWidth}</li>
+                        <li>Before Image Offset Height: {beforeImage.offsetHeight}</li>
+                    </ul>
+                    <div className="et-l">
+                        <div className={"twentytwenty-wrapper twentytwenty-horizontal et_pb_image_wrap " + sizeClass} style={styles.wrapper}>
+                            <div className="twentytwenty-container" style={styles.container} ref={this.container}>
+                                <img
+                                    onLoad={this.onBeforeImgLoad}
                                     src={beforeImage.src}
-                                    alt=""
+                                    alt="Before Image"
                                     className="twentytwenty-before"
                                     style={styles.beforeImage}
                                     width={selectedWidth}
                                     height={selectedHeight}
                                 />
-                                <Image
-                                    onLoad = {this.onAfterImgLoad}
+                                <img
+                                    onLoad={this.onAfterImgLoad}
                                     src={afterImage.src}
                                     alt=""
                                     className="twentytwenty-after"
