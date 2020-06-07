9519437cb031ca772ebb14f6fdf5ce28f1ffea41
Aaron Bolton <aaron@boltonstudios.com>
2020-06-07 18:57:06 -0400

    Releasing version 1.3.4.

--- before-after-image-extension.php
+++ before-after-image-extension.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Before + After Images for Divi
 Description: Create before-and-after image sliders in the Divi Visual Builder easily.
-Version:     1.3.3
+Version:     1.3.4
 Author:      Aaron Bolton
 Author URI:  https://www.boltonstudios.com
 License:     GPL2
