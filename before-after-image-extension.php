801cafd7bdeb64bf8af1128c728f1b9ea139cc67
Ghost Writer <4065416+ghost-writer-727@users.noreply.github.com>
2024-05-21 12:58:47 -0400

    Bump up version number

--- before-after-image-extension.php
+++ before-after-image-extension.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Before + After Images for Divi
 Description: Create before-and-after image sliders in the Divi Visual Builder easily.
-Version:     1.3.5.2
+Version:     1.3.5.3
 Author:      Aaron Bolton + Daniel Purifoy
 Author URI:  https://www.boltonstudios.com
 License:     GPL2
